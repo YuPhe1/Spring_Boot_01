c3ca3df2bbeef0ee905a4c378ade09c3e4199171
YuPhe1 <oksunboy1@naver.com>
2023-09-21 11:23:33 +0900

    DTO출력 방법

--- src/main/java/com/example/demo/controller/ReqController.java
+++ src/main/java/com/example/demo/controller/ReqController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.DemoDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,28 +12,39 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ReqController {
 
     @GetMapping("/req1")
-    public String req1(@RequestParam("p1") String p1, @RequestParam("p2") int p2){
+    public String req1(@RequestParam("p1") String p1, @RequestParam("p2") int p2) {
         System.out.println("p1 = " + p1 + ", p2 = " + p2);
         return "index";
     }
 
     // /req2/aaa
     @GetMapping("/req2/{p3}")
-    public String req2(@PathVariable("p3") String p3){
+    public String req2(@PathVariable("p3") String p3) {
         System.out.println("p3 = " + p3);
         return "index";
     }
 
     @PostMapping("/req3")
-    public String req3(@RequestParam("name") String name, @RequestParam("age") int age){
+    public String req3(@RequestParam("name") String name, @RequestParam("age") int age) {
         System.out.println("name = " + name + ", age = " + age);
         return "index";
     }
 
     @GetMapping("/req4")
-    public String req4(Model model){
+    public String req4(Model model) {
         model.addAttribute("a1", "안녕하세요");
         model.addAttribute("a2", "반갑습니다.");
         return "req4";
     }
+
+    @GetMapping("/req5")
+    public String req5(Model model) {
+        DemoDTO demoDTO = new DemoDTO();
+        demoDTO.setId(1L);
+        demoDTO.setName("이름");
+        demoDTO.setAge(11);
+        model.addAttribute("demo", demoDTO);
+        return "req5";
+    }
 }
+
